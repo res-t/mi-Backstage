7c27badbeb5347994239d8085cb39b33b4c731e1
yanggen <yanggen.@qq.com>
2018-11-07 20:20:05 +0800

    Update router/index.js

--- router/index.js
+++ router/index.js
@@ -15,4 +15,25 @@ router.get('/phone',(req,res)=>{
         res.send(result);
     })
 })
+router.get('/tvbox',(req,res)=>{
+    var sql = "SELECT * FROM `t_mi_type` WHERE parent_id=2";
+    pool.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+router.get('/loptop',(req,res)=>{
+    var sql = "SELECT * FROM `t_mi_type` WHERE parent_id=3";
+    pool.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+router.get('/patch',(req,res)=>{
+    var sql = "SELECT * FROM `t_mi_type` WHERE parent_id=4";
+    pool.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
 module.exports =router;
